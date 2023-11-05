3592344332ced804581519e125e26664270b779b
PointThink <erykd35@gmail.com>
2023-11-05 15:13:58 +0100

    Seperated entry point from the rest to prevent multiple definition

--- Engine/Source/CatWare.h
+++ Engine/Source/CatWare.h
@@ -6,7 +6,6 @@
 
 #include "CatWare/Core.h"
 #include "CatWare/Application.h"
-#include "CatWare/EntryPoint.h"
 #include "CatWare/Utils/Log.h"
 #include "CatWare/Graphics/Window.h"
 #include "CatWare/Input/KeyboardAndMouse.h"

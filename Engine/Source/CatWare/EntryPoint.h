a2fbeb27b77f5d5aba46e1f4a58d002a0e635c74
PointThink <erykd35@gmail.com>
2023-11-05 15:02:59 +0100

    Fixed multiple entry point definition

--- Engine/Source/CatWare/EntryPoint.h
+++ Engine/Source/CatWare/EntryPoint.h
@@ -5,6 +5,8 @@
 
 #define CW_REGISTER_APP(AppClass) CatWare::Application* CatWare::CreateApplication( ) { return new AppClass; }
 
+
+
 namespace CatWare
 {
 	extern Application* CreateApplication( );
@@ -12,9 +14,14 @@ namespace CatWare
 
 // TODO: Make the entry point use WinMain and windows subsystem in release/distrubution mode
 
-#if defined(CW_PLATFORM_WIN64)
-	int main( int argc, char** argv )
-	{
-		CatWare::CreateApplication( )->Run( );
-	}
+#if !defined(CW_ENTRYPOINT_CREATED)
+#	if defined(CW_PLATFORM_WIN64)
+		int main( int argc, char** argv )
+		{
+			CatWare::CreateApplication( )->Run( );
+		}
+#	endif
+
+#	define CW_ENTRYPOINT_CREATED
+
 #endif

9c18c166cc77f6a0b6ef51b437fda00db2243d00
PointThink <69512713+pointThink@users.noreply.github.com>
2023-12-02 20:54:33 +0100

    Error message boxes for linux

--- Engine/Source/CatWare/Graphics/Window.cpp
+++ Engine/Source/CatWare/Graphics/Window.cpp
@@ -12,11 +12,16 @@
 
 namespace CatWare
 {
-	Window::Window( std::string title, unsigned int width, unsigned int height, bool isFullscreen )
+	Window::Window( const std::string& title, const unsigned int width, const unsigned int height, const bool isFullscreen )
 	{
 		CW_ENGINE_LOG->Info( "Creating %dx%d window with title %s", width, height, title.c_str( ) );
 
-		sdlWindow = SDL_CreateWindow( title.c_str( ), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL );
+		sdlWindow = SDL_CreateWindow(
+			title.c_str( ),
+			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			width, height,
+			SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+			);
 
 		SetFullscreen( isFullscreen );
 
@@ -50,30 +55,30 @@ namespace CatWare
 		{
 			switch ( e.type )
 			{
-			case SDL_QUIT:
-				shouldClose = true;
-				break;
+				case SDL_QUIT:
+					shouldClose = true;
+					break;
 
 
-			case SDL_KEYDOWN:
-				Input::SetKeyPressed( e.key.keysym.scancode, true );
-				break;
+				case SDL_KEYDOWN:
+					Input::SetKeyPressed( e.key.keysym.scancode, true );
+					break;
 
-			case SDL_KEYUP:
-				Input::SetKeyPressed( e.key.keysym.scancode, false );
-				break;
+				case SDL_KEYUP:
+					Input::SetKeyPressed( e.key.keysym.scancode, false );
+					break;
 
-			case SDL_MOUSEBUTTONDOWN:
-				Input::SetMousePressed( e.button.button, true );
-				break;
+				case SDL_MOUSEBUTTONDOWN:
+					Input::SetMousePressed( e.button.button, true );
+					break;
 
-			case SDL_MOUSEBUTTONUP:
-				Input::SetMousePressed( e.button.button, false );
-				break;
+				case SDL_MOUSEBUTTONUP:
+					Input::SetMousePressed( e.button.button, false );
+					break;
 
 
-			case SDL_MOUSEMOTION:
-				Input::SetMouseMotion( { ( double ) e.motion.x, ( double ) e.motion.y } );
+				case SDL_MOUSEMOTION:
+					Input::SetMouseMotion( { ( double ) e.motion.x, ( double ) e.motion.y } );
 			}
 		}
 	}
@@ -137,6 +142,6 @@ namespace CatWare
 
 	bool Window::GetCursorLocked( ) { return isCursorLocked; }
 
-	
+
 	bool Window::ShouldClose( ) { return shouldClose; }
 }

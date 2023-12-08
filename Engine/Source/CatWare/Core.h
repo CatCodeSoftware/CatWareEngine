913145b45b876781440f56cf106ab609f1d068a9
pointThink <pointthink@protonmail.com>
2023-12-08 14:32:08 +0100

    Probably fixed imgui

--- Engine/Source/CatWare/Core.h
+++ Engine/Source/CatWare/Core.h
@@ -15,8 +15,10 @@
 #if defined(CW_PLATFORM_WIN64)
 #	if defined(CATWARE_BUILD)
 #		define CATWARE_API __declspec(dllexport)
+#		define IMGUI_API __declspec(dllexport)
 #	else
 #		define CATWARE_API __declspec(dllimport)
+#		define IMGUI_API __declspec(dllimport)
 #	endif
 #elif defined(CW_PLATFORM_UNIX64)
 #   define CATWARE_API
